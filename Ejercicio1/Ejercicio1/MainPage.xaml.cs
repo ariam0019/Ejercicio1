d03d8ac404025d64ad0af3dd740151a42ac5bb9f
ariam0019 <ariamenamorado00@gmail.com>
2022-12-01 21:39:00 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Ejercicio1/MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btnsuma_Clicked(object sender, EventArgs e)
+        {
+            var vari = new Model.Variables
+            {
+                n1 = Convert.ToInt32(txtnumero1.Text),
+                n2 = Convert.ToInt32(txtnumero2.Text),
+                Resultado = Convert.ToInt32(txtnumero1.Text) + Convert.ToInt32(txtnumero2.Text)
+            };
+            var ma = new Views.Page1();
+            ma.BindingContext = vari;
+            await Navigation.PushModalAsync(ma);
+        }
+
+        private async void Btnresta_Clicked(object sender, EventArgs e)
+        {
+            var vari = new Model.Variables
+            {
+                n1 = Convert.ToInt32(txtnumero1.Text),
+                n2 = Convert.ToInt32(txtnumero2.Text),
+                Resultado = Convert.ToInt32(txtnumero1.Text) - Convert.ToInt32(txtnumero2.Text)
+            };
+            var ma = new Views.Page1();
+            ma.BindingContext = vari;
+            await Navigation.PushModalAsync(ma);
+        }
+
+        private async void Btnmultiplicacion_Clicked(object sender, EventArgs e)
+        {
+            var vari = new Model.Variables
+            {
+                n1 = Convert.ToInt32(txtnumero1.Text),
+                n2 = Convert.ToInt32(txtnumero2.Text),
+                Resultado = Convert.ToInt32(txtnumero1.Text) * Convert.ToInt32(txtnumero2.Text)
+            };
+            var ma = new Views.Page1();
+            ma.BindingContext = vari;
+            await Navigation.PushModalAsync(ma);
+        }
+
+        private async void Btndivision_Clicked(object sender, EventArgs e)
+        {
+            var vari = new Model.Variables
+            {
+                n1 = Convert.ToInt32(txtnumero1.Text),
+                n2 = Convert.ToInt32(txtnumero2.Text),
+                Resultado = Convert.ToInt32(txtnumero1.Text) / Convert.ToInt32(txtnumero2.Text)
+            };
+            var ma = new Views.Page1();
+            ma.BindingContext = vari;
+            await Navigation.PushModalAsync(ma);
+        }
+    }
+}
---
+++ Ejercicio1/Ejercicio1/Model/Variables.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio1.Model
+{
+    public class Variables
+    {
+        public int n1 { get; set; } 
+        public int n2 { get; set; } 
+        public int Resultado { get; set; }  
+    }
+}
